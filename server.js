269deeda6fdd25aa79ba01c1c9f803be81512ae8
myName <myemail@gmail.com>
2018-01-07 02:46:08 +0600

    Hello!!

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now} ${req.method} ${req.url}`;
+
+	fs.appendFileSync('server.log', log + '\n');
+	console.log();
+	next();
+});
+
+app.use((req, res, next) => {
+	res.render('maintenance.hbs');
+	next(); // no next, all blocked next;
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Welcome to my site'
+	});
+
+
+	// res.send('<h1>Hello</h1>');
+	/*res.send({
+		name: 'Bello',
+		links: [
+			'Google',
+			'Baalo'
+		]
+	});*/
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About page'
+	});
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		errMsg: 'Unable to get any!'
+	});
+});
+
+app.listen(3000, () => {
+	console.log('Server is at port 3000');
+});
